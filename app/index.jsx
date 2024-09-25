dd0990533c538584e5e46c88907d3cdcfd31adcb
karl <karlkivisild@gmail.com>
2024-09-25 11:11:02 +0300

    Added new assets, updated packages

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl text-red-400">Aora</Text>
-      <StatusBar style="auto" />
+    <Text className="text-3xl text-red-400">Aora</Text>
+    <StatusBar style="auto" />
+    <Link className="font-pblack" href="/profile">
+      Go To Profile
+    </Link>
     </View>
   );
 }

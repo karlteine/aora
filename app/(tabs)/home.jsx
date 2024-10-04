66f75c6698bffda738e30e6e8324176f12aec116
karl <karlkivisild@gmail.com>
2024-10-04 17:20:48 +0300

    Code fixes

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,66 +1,111 @@
-import { useState } from 'react';
-import { FlatList, Image, RefreshControl, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
+import { useLocalSearchParams } from 'expo-router';
 
-import { EmptyState, SearchInput, Trending, VideoCard } from '@components';
+
 import { images } from '@constants';
-import { useGlobalContext } from '@context/GlobalProvider';
+import SearchInput from '@components/SearchInput';
+import Trending from '@components/Trending';
+import EmptyState from '@components/EmptyState';
 import { getAllPosts, getLatestPosts } from '@lib/appwrite';
-import useAppwrite from '@lib/useAppWrite';
+import useAppWrite from '@lib/useAppWrite';
+import VideoCard from '@components/VideoCard';
+import { useGlobalContext } from '@context/GlobalProvider';
+
 
 const Home = () => {
-  const { data: posts, refetch } = useAppwrite(getAllPosts);
-  const { data: latestPosts } = useAppwrite(getLatestPosts);
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  
+  const { data: posts, refetch: refetchPosts } = useAppWrite(getAllPosts);
 
-  const { user } = useGlobalContext();
+  const { data: latestposts ,refetch: refetchLatestPosts} = useAppWrite(getLatestPosts);
 
-  const [refreshing, setRefreshing] = useState(false);
+  const [refreshing, setRefreshing] = useState(false)
 
+  const { refresh } = useLocalSearchParams(); 
+
+  
   const onRefresh = async () => {
-    setRefreshing(true);
-    await refetch();
-    setRefreshing(false);
-  };
+    setRefreshing(true)
+    await Promise.all([refetchPosts(), refetchLatestPosts()]);
+    
+    setRefreshing(false)
+  }
 
-  // one flatlist
-  // with list header
-  // and horizontal flatlist
+  useFocusEffect(
+    useCallback(() => {
+      if (refresh) {
+        onRefresh();
+      }
+    }, [refresh])
+  );
 
-  //  we cannot do that with just scrollview as there's both horizontal and vertical scroll (two flat lists, within trending)
+
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={posts}
-        keyExtractor={item => item.$id}
-        renderItem={({ item }) => <VideoCard title={item.title} thumbnail={item.thumbnail} video={item.video} creator={item.creator.username} avatar={item.creator.avatar} />}
+        keyExtractor={( item ) => item.$id}
+        
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title} // Title of the video post
+            creator={item.creator?.username} // Creator's username
+            avatar={item.creator?.avatar} // Avatar image URL
+            thumbnail={item.thumbnail} // Thumbnail image URL
+            video={item.video} // Video URL
+            videoid={item.$id} // Explicitly passing the video ID
+            onUpdate={onRefresh} // Function to call on update
+        />
+        )}
         ListHeaderComponent={() => (
-          <View className="flex my-6 px-4 space-y-6">
-            <View className="flex justify-between items-start flex-row mb-6">
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100">Welcome back,</Text>
-                <Text className="text-2xl font-psemibold text-white">{user?.username || ''}</Text>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {user?.username}
+                </Text>
               </View>
-
               <View className="mt-1.5">
-                <Image source={images.logoSmall} className="w-9 h-10" resizeMode="contain" />
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
               </View>
             </View>
 
-            <SearchInput />
+            <SearchInput title="Search for a video topic" />
 
             <View className="w-full flex-1 pt-5 pb-8">
-              <Text className="text-lg font-pregular text-gray-100 mb-3">Latest Videos</Text>
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
 
-              <Trending posts={latestPosts ?? []} />
+              <Trending posts={latestposts ?? []} />
             </View>
           </View>
         )}
-        ListEmptyComponent={() => <EmptyState title="No Videos Found" subtitle="No videos created yet" />}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        ListEmptyComponent={() => (
+         <EmptyState
+          title="No Videos Found"
+          subtitle="Be the first one to upload a video"
+         />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
+      
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,53 +1,105 @@
-import { router } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Image, FlatList, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { icons } from '@constants';
-import useAppwrite from '@lib/useAppWrite';
-import { getUserPosts, signOut } from '@lib/appwrite';
+import { images, icons } from '@constants';
+import EmptyState from '@components/EmptyState';
+import { getUserPosts } from '@lib/appwrite';
+import useAppWrite from '@lib/useAppWrite';
+import VideoCard from '@components/VideoCard';
+import InfoBox from '@components/InfoBox';
+import { router } from 'expo-router';
 import { useGlobalContext } from '@context/GlobalProvider';
-import { EmptyState, InfoBox, VideoCard } from '@components';
+import { TouchableOpacity } from 'react-native';
+import { signOut } from '@lib/appwrite';
+
+
 
 const Profile = () => {
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
-  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
-
+  const { data: posts , refetch} = useAppWrite(() => getUserPosts(user.$id));
+  const [refreshing, setRefreshing] = useState(false)
   const logout = async () => {
-    await signOut();
-    setUser(null);
-    setIsLoggedIn(false);
+    await signOut();  
 
-    router.replace('/sign-in');
-  };
+    router.replace("/sign-in");
+  } 
 
+  
+  const onRefresh = async () => {
+    setRefreshing(true)
+    await refetch();
+    setRefreshing(false)
+  }
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={posts}
-        keyExtractor={item => item.$id}
-        renderItem={({ item }) => <VideoCard title={item.title} thumbnail={item.thumbnail} video={item.video} creator={item.creator.username} avatar={item.creator.avatar} />}
-        ListEmptyComponent={() => <EmptyState title="No Videos Found" subtitle="No videos found for this profile" />}
+        keyExtractor={( item ) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title} // Title of the video post
+            creator={item.creator?.username} // Creator's username
+            avatar={item.creator?.avatar} // Avatar image URL
+            thumbnail={item.thumbnail} // Thumbnail image URL
+            video={item.video} // Video URL
+          />
+        )}
         ListHeaderComponent={() => (
           <View className="w-full flex justify-center items-center mt-6 mb-12 px-4">
-            <TouchableOpacity onPress={logout} className="flex w-full items-end mb-10">
-              <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" />
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={logout}
+            className="flex w-full items-end mb-10"
+          >
+            <Image
+              source={icons.logout}
+              resizeMode="contain"
+              className="w-6 h-6"
+            />
+          </TouchableOpacity>
 
-            <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
-              <Image source={{ uri: user?.avatar }} className="w-[90%] h-[90%] rounded-lg" resizeMode="cover" />
-            </View>
-
-            <InfoBox title={user?.username} containerStyles="mt-5 mb-5" titleStyles="text-lg" />
-
-            <View className="mt-5 flex flex-row">
-              <InfoBox title={posts.length || 0} subtitle="Posts" titleStyles="text-xl" containerStyles="mr-10" />
-              <InfoBox title="2.8k" subtitle="Followers" titleStyles="text-xl" />
-            </View>
+          <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="w-[90%] h-[90%] rounded-lg"
+              resizeMode="cover"
+            />
           </View>
-        )}
-      />
-    </SafeAreaView>
-  );
-};
 
-export default Profile;
+          <InfoBox
+            title={user?.username}
+            containerStyles="mt-5"
+            titleStyles="text-lg"
+          />
+
+          <View className="mt-5 flex flex-row">
+            <InfoBox
+              title={posts.length || 0}
+              subtitle="Posts"
+              titleStyles="text-xl"
+              containerStyles="mr-10"
+            />
+            <InfoBox
+              title="1.2k"
+              subtitle="Followers"
+              titleStyles="text-xl"
+            />
+          </View>
+        </View>
+        )}
+        ListEmptyComponent={() => (
+         <EmptyState
+          title="No Videos Found"
+          subtitle="No Videos found for this search"
+         />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+      
+    </SafeAreaView>
+  )
+}
+
+export default Profile
